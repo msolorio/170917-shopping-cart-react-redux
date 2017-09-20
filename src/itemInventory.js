68688ce87f1833f7e9dd3bf20da25c41d89ce8df
msolorio <msolorio789@gmail.com>
2017-09-19 22:54:49 -0700

    add to item inventory

--- src/itemInventory.js
+++ src/itemInventory.js
@@ -16,6 +16,18 @@ const itemInventory = [
     price: 250,
     ratingStars: 4.5,
     itemId: 2
+  },
+  {
+    itemName: 'Pocket Hammond Organ',
+    price: 300,
+    ratingStars: 4.5,
+    itemId: 3
+  },
+  {
+    itemName: 'Mini-Fridge',
+    price: 80,
+    ratingStars: 4.3,
+    itemId: 4
   }
 ];
 
